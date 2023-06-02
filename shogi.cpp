5af3b59171340e92d9fb284acaacace3b226303c
KeitaroHamada <hamataro1029@gmail.com>
2023-06-02 14:17:50 +0900

    ini

---
+++ shogi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+class Koma{
+    private:
+        int x;
+        int y;
+        string name;
+    public:
+        setKoma(int x,int y,string name){
+            this->x=x;
+            this->y=y;
+            this->name=name;
+        }
+        virtual 
+}
+class Hu{
+
+}
+class showBan{
+
+};
