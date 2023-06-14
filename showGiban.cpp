16d405cea050ef2de91375100bcd37eb839206cd
KeitaroHamada <hamataro1029@gmail.com>
2023-06-14 17:03:12 +0900

    うーーむ

--- showGiban.cpp
+++ showGiban.cpp
@@ -13,21 +13,16 @@ class Field{
         vector<Koma>koma;
     public:
         void showField(){
-            Koma first[9];
-            Koma second[9];
-            Koma third[9];
-            Koma forth[9];
-            Koma sixth[9];
-            Koma seventh[9];
-            Koma eighth[9];
-            Koma ninth[9];
+            Koma first[9][9];
 
             cout << "９８７６５４３２１ \n";
             for(int i=0;i<koma.size();i++){
                 switch(koma[i].getx()){
                     case 0 : 
                         for(int j=0;j<9;j++){
-
+                            if(koma[i].gety()==j){
+                                first[j] = 
+                            }
                         }
                 }
             }
