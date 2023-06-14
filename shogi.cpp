489e58f1f1d76d9438c2da35a1a1e305e6d070a1
KairiGit <r04i39@ed.cc.suzuka-ct.ac.jp>
2023-06-14 23:57:36 +0900

    Koma型のポインタa,bを受け取り、aの名前にはにe(empty)を、bの名前にはaの名前を入れる

--- shogi.cpp
+++ shogi.cpp
@@ -3,21 +3,42 @@
 using namespace std;
 class Koma{
     private:
-        int x;
-        int y;
-        string name;
+        char name;
     public:
-        setKoma(int x,int y,string name){
-            this->x=x;
-            this->y=y;
-            this->name=name;
+        Koma(){}
+        Koma(char n){
+            name = n;
         }
-
-        virtual 
-}
-class Hu{
-
-}
-class showBan{
-
+        char getname(){return name;}
+        void setname(char n){name = n;}
 };
+void Move(Koma* a,Koma* b,char* motigoma){
+    char tmpa = a->getname();
+    char tmpb = b->getname();
+    a->setname('e');
+    b->setname(tmpa);
+
+    if(tmpb!='e'&&tmpb!='o'){//移動先を持ち駒に入れる
+        *motigoma = tmpb;
+    }
+    if(tmpb=='o'){
+        cout <<"You Win!"<<endl;
+    }
+}
+int main(){
+    Koma ban[9][9];
+    Koma a('h');
+    Koma b('k');
+    Koma c('o');
+    char motigoma='m';
+
+    ban[0][3] = a;
+    ban[0][2] = b;
+    ban[0][1] = c;
+
+    Move(&a,&b,&motigoma);
+    cout << a.getname()<<b.getname()<<c.getname()<<motigoma<<endl;
+    Move(&b,&c,&motigoma);
+    cout << a.getname()<<b.getname()<<c.getname()<<motigoma<<endl;
+
+}
