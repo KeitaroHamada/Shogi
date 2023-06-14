f91192494c1079161366e77bebcf7d42a1ae9b69
KeitaroHamada <hamataro1029@gmail.com>
2023-06-14 16:56:49 +0900

    途中

--- showGiban.cpp
+++ showGiban.cpp
@@ -1,23 +1,36 @@
 #include<iostream>
 #include<cstring>
+#include<vector>
 using namespace std;
+enum Kansuuji{一,二,三,四,五,六,七,八,九};
 class Koma{
-    private:
-        int x;
-        int y;
-        string name;
     public:
-        setKoma(int x,int y,string name){
-            this->x=x;
-            this->y=y;
-            this->name=name;
+    int getx();
+    int gety();
+};
+class Field{
+    private:
+        vector<Koma>koma;
+    public:
+        void showField(){
+            Koma first[9];
+            Koma second[9];
+            Koma third[9];
+            Koma forth[9];
+            Koma sixth[9];
+            Koma seventh[9];
+            Koma eighth[9];
+            Koma ninth[9];
+
+            cout << "９８７６５４３２１ \n";
+            for(int i=0;i<koma.size();i++){
+                switch(koma[i].getx()){
+                    case 0 : 
+                        for(int j=0;j<9;j++){
+
+                        }
+                }
+            }
+
         }
-
-        virtual 
-}
-class Hu{
-
-}
-class showBan{
-
 };
