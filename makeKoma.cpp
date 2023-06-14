9d4f13dc60adf0444734c022f8fc6392d9ba525b
masuyayout <m40ta2726@icloud.com>
2023-06-15 00:05:58 +0900

    作り直し１

--- makeKoma.cpp
+++ makeKoma.cpp
@@ -6,16 +6,20 @@ enum Role{Hu, Hisya, Kaku, Kyosya, Keima, Gin, Kin, Ou, Null};
 class Koma{
     private:
         int x,y;
-        bool Nalustate;
-        char k[9][9]; //どこで定義するの？
+        bool isAlive;
+        bool naruState;
         Team team;
         Role role;
+        string name;
     public:
         int getx(){
             return x;
         };
         int gety(){
             return y;
+        };
+        string getName(string n){
+            return n;
         };
         void Steel(Team t){
             if(t==team1){
@@ -24,122 +28,14 @@ class Koma{
                 t=team2;
             }
         };
-        void isNaru(bool n, Role r){
-            if(n){
-                switch(r){
-                    case Hu     : k[x][y]='と';break;
-                    case Hisya  : k[x][y]='龍';break;
-                    case Kaku   : k[x][y]='馬';break;
-                    case Kyosya : k[x][y]='金';break;
-                    case Keima  : k[x][y]='金';break;
-                    case Gin    : k[x][y]='金';break;
-                    case Kin    : k[x][y]='金';break;
-                    case Ou     : k[x][y]='王';break;
-                    case Null   : k[x][y]='　';break;
-                }
+        void isNaru(Team t,int y){
+            if(t==team1&&y<=2){
+                naruState=true;
+            }else if(t==team2&&y<=2){
+                naruState=false;
             }
         };
         void setPosition(){
-            switch(x){
-                case 0 : switch(y){
-                    case 0 : k[x][y]='香';break;
-                    case 1 : k[x][y]='桂';break;
-                    case 2 : k[x][y]='銀';break;
-                    case 3 : k[x][y]='金';break;
-                    case 4 : k[x][y]='王';break;
-                    case 5 : k[x][y]='金';break;
-                    case 6 : k[x][y]='銀';break;
-                    case 7 : k[x][y]='桂';break;
-                    case 8 : k[x][y]='香';break;
-                }
-                case 1 : switch(y){
-                    case 0 : k[x][y]='　';break;
-                    case 1 : k[x][y]='飛';break;
-                    case 2 : k[x][y]='　';break;
-                    case 3 : k[x][y]='　';break;
-                    case 4 : k[x][y]='　';break;
-                    case 5 : k[x][y]='　';break;
-                    case 6 : k[x][y]='　';break;
-                    case 7 : k[x][y]='角';break;
-                    case 8 : k[x][y]='　';break;
-                }
-                case 2 : switch(y){
-                    case 0 : k[x][y]='歩';break;
-                    case 1 : k[x][y]='歩';break;
-                    case 2 : k[x][y]='歩';break;
-                    case 3 : k[x][y]='歩';break;
-                    case 4 : k[x][y]='歩';break;
-                    case 5 : k[x][y]='歩';break;
-                    case 6 : k[x][y]='歩';break;
-                    case 7 : k[x][y]='歩';break;
-                    case 8 : k[x][y]='歩';break;
-                }
-                case 3 : switch(y){
-                    case 0 : k[x][y]='　';break;
-                    case 1 : k[x][y]='　';break;
-                    case 2 : k[x][y]='　';break;
-                    case 3 : k[x][y]='　';break;
-                    case 4 : k[x][y]='　';break;
-                    case 5 : k[x][y]='　';break;
-                    case 6 : k[x][y]='　';break;
-                    case 7 : k[x][y]='　';break;
-                    case 8 : k[x][y]='　';break;
-                }
-                case 4 : switch(y){
-                    case 0 : k[x][y]='　';break;
-                    case 1 : k[x][y]='　';break;
-                    case 2 : k[x][y]='　';break;
-                    case 3 : k[x][y]='　';break;
-                    case 4 : k[x][y]='　';break;
-                    case 5 : k[x][y]='　';break;
-                    case 6 : k[x][y]='　';break;
-                    case 7 : k[x][y]='　';break;
-                    case 8 : k[x][y]='　';break;
-                }
-                case 5 : switch(y){
-                    case 0 : k[x][y]='　';break;
-                    case 1 : k[x][y]='　';break;
-                    case 2 : k[x][y]='　';break;
-                    case 3 : k[x][y]='　';break;
-                    case 4 : k[x][y]='　';break;
-                    case 5 : k[x][y]='　';break;
-                    case 6 : k[x][y]='　';break;
-                    case 7 : k[x][y]='　';break;
-                    case 8 : k[x][y]='　';break;
-                }
-                case 6 : switch(y){
-                    case 0 : k[x][y]='歩';break;
-                    case 1 : k[x][y]='歩';break;
-                    case 2 : k[x][y]='歩';break;
-                    case 3 : k[x][y]='歩';break;
-                    case 4 : k[x][y]='歩';break;
-                    case 5 : k[x][y]='歩';break;
-                    case 6 : k[x][y]='歩';break;
-                    case 7 : k[x][y]='歩';break;
-                    case 8 : k[x][y]='歩';break;
-                }
-                case 7 : switch(y){
-                    case 0 : k[x][y]='　';break;
-                    case 1 : k[x][y]='角';break;
-                    case 2 : k[x][y]='　';break;
-                    case 3 : k[x][y]='　';break;
-                    case 4 : k[x][y]='　';break;
-                    case 5 : k[x][y]='　';break;
-                    case 6 : k[x][y]='　';break;
-                    case 7 : k[x][y]='飛';break;
-                    case 8 : k[x][y]='　';break;
-                }
-                case 8 : switch(y){
-                    case 0 : k[x][y]='香';break;
-                    case 1 : k[x][y]='桂';break;
-                    case 2 : k[x][y]='銀';break;
-                    case 3 : k[x][y]='金';break;
-                    case 4 : k[x][y]='王';break;
-                    case 5 : k[x][y]='金';break;
-                    case 6 : k[x][y]='銀';break;
-                    case 7 : k[x][y]='桂';break;
-                    case 8 : k[x][y]='香';break;
-                }
-            }
-        };
+            
+        }
 };
