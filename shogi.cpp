3af1392cf7f1b5e8a3f08a0049691608f391f121
KairiGit <130728369+KairiGit@users.noreply.github.com>
2023-06-15 10:16:25 +0900

    Koma型に変更

--- shogi.cpp
+++ shogi.cpp
@@ -11,17 +11,20 @@ class Koma{
         }
         char getname(){return name;}
         void setname(char n){name = n;}
+        void copy(Koma x){
+            name = x.getname();
+        }
 };
-void Move(Koma* a,Koma* b,char* motigoma){
-    char tmpa = a->getname();
-    char tmpb = b->getname();
-    a->setname('e');
-    b->setname(tmpa);
+void Move(Koma* a,Koma* b,Koma* motigoma){
+    Koma tmpa(a->getname());
+    Koma tmpb(b->getname());
+    a->copy(tmpb);
+    b->copy(tmpa);
 
-    if(tmpb!='e'&&tmpb!='o'){//移動先を持ち駒に入れる
-        *motigoma = tmpb;
+    if(tmpb.getname()!='e'&&tmpb.getname()!='o'){//移動先を持ち駒に入れる
+        motigoma copy(tmpb);
     }
-    if(tmpb=='o'){
+    if(tmpb.getname=='o'){
         cout <<"You Win!"<<endl;
     }
 }
@@ -30,15 +33,15 @@ int main(){
     Koma a('h');
     Koma b('k');
     Koma c('o');
-    char motigoma='m';
+    Koma motigoma('m');
 
     ban[0][3] = a;
     ban[0][2] = b;
     ban[0][1] = c;
 
     Move(&a,&b,&motigoma);
-    cout << a.getname()<<b.getname()<<c.getname()<<motigoma<<endl;
+    cout << a.getname()<<b.getname()<<c.getname()<<endl;
     Move(&b,&c,&motigoma);
-    cout << a.getname()<<b.getname()<<c.getname()<<motigoma<<endl;
+    cout << a.getname()<<b.getname()<<c.getname()<<endl;
 
 }
