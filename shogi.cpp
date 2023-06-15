72a74a4f40c64537199982f575e635be806e4ff7
KairiGit <r04i39@ed.cc.suzuka-ct.ac.jp>
2023-06-15 23:08:43 +0900

    ittan

--- shogi.cpp
+++ shogi.cpp
@@ -1,30 +1,74 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
+enum Team{team1, team2, nothing};
+enum Role{Hu, Hisya, Kaku, Kyosya, Keima, Gin, Kin, Ou, Null};
 class Koma{
     private:
-        char name;
+        int x,y;
+        bool isAlive;
+        bool naruState;
+        Team team;
+        Role role;
+        string name;
     public:
-        Koma(){}
-        Koma(char n){
-            name = n;
+        void setPosition(int x, int y){
+            this->x=x;
+            this->y=y;
         }
-        char getname(){return name;}
-        void setname(char n){name = n;}
+        int getx(){
+            return x;
+        }
+        int gety(){
+            return y;
+        }
+        string getname(){
+            return name;
+        };
+        void Stolen(Team wit){
+                wit=nothing;
+        }
+        bool isNaru(Team t,int y){
+            if((t==team1&&y<=2)||(t==team2&&y>=6)){
+                naruState=true;
+            }else{
+                naruState=false;
+            }
+            return naruState;
+        }
 };
-void Move(Koma* a,Koma* b,char* motigoma){
-    char tmpa = a->getname();
-    char tmpb = b->getname();
-    a->setname('e');
-    b->setname(tmpa);
 
-    if(tmpb!='e'&&tmpb!='o'){//移動先を持ち駒に入れる
+void Move(Koma* A,Koma* a,Koma* B,Koma* b){
+
+    
+    int tmpAx = A->getx();
+    int tmpAy = A->gety();
+
+    int tmpax = a->getx();
+    int tmpay = a->gety();
+
+    A->setPosition(B->getx(),B->gety());
+    B->setPosition(tmpAx,tmpAy);
+
+    a->setPosition(b->getx(),b->gety());
+    b->setPosition(tmpax,tmpay);
+
+    if(B->isAlive()){
+        Role tmpBR = B->getRole();
+        B->setName(b->getRole());
+        b->setName(tmpBR);
+        B->Stolen();
+        b->put();
+    }
+}
+
+if(tmpb!='e'&&tmpb!='o'){//移動先を持ち駒に入れる
         *motigoma = tmpb;
     }
     if(tmpb=='o'){
         cout <<"You Win!"<<endl;
     }
-}
+
 int main(){
     Koma ban[9][9];
     Koma a('h');
