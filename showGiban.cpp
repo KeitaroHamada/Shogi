8fb9a872666471af061fde0c0ee4fc780ea42d08
KeitaroHamada <hamataro1029@gmail.com>
2023-06-15 21:06:12 +0900

    補足

--- showGiban.cpp
+++ showGiban.cpp
@@ -7,7 +7,7 @@ class Field{
     private:
         vector<Koma>koma;//長さを自由に変えられるKoma型の配列のようなもの
     public:
-        void showField(){ //将棋盤を出力する関数、class Komaにはgetx(),gety(),getName()が必要
+        void showField(){ //将棋盤を出力する関数、class Komaにはgetx(),gety(),getName(),getTeam()が必要
             string kansuuji[9]={"一","二","三","四","五","六","七","八","九"};
             Koma k[9][9];
             for(int i=0;i<koma.size();i++){
