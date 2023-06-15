d5e55f9294adbbe8dfec3a22249632d643f227c5
KeitaroHamada <hamataro1029@gmail.com>
2023-06-15 22:17:29 +0900

    brindField()の実装

--- showGiban.cpp
+++ showGiban.cpp
@@ -24,4 +24,10 @@ class Field{
                 cout << kansuuji[j] << endl;
             }
         }
+        void brindField(){
+            Koma b[9][9];
+            for(int i=0;i<koma.size();i++){
+                b[koma[i].getx()][koma[i].gety()]=koma[i];
+            }
+        }
 };
