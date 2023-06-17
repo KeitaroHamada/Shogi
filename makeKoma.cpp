2c70038c1b3fb63034d28f468fd00c45df6ee278
masuyayout <m40ta2726@icluod.com>
2023-06-17 20:30:53 +0900

    81個の駒を区別する方法を考えてる。あと持ち駒が盤に出されたときにfalseを返す関数作ったよ

--- makeKoma.cpp
+++ makeKoma.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 enum Team{team1, team2};
 enum Role{Hu, Hisya, Kaku, Kyosya, Keima, Gin, Kin, Ou, Null};
+enum a{};//81個の駒を区別する何かが欲しい(検討加速中)
 class Koma{
     private:
         int x,y;
@@ -12,11 +13,11 @@ class Koma{
         Role role;
         string name;
     public:
-        void setPosition(int x, int y){
+        void setPosition(int x, int y){//駒の初期位置を設定する関数
             this->x=x;
             this->y=y;
         }
-        void setName(Role r){
+        void setName(Role r){//駒の名前を設定する関数
             role=r;
             switch(r){
                     case Hu     : name='歩';break;
@@ -30,34 +31,37 @@ class Koma{
                     case Null   : name='　';break;
                 }
         }
-        void setTaem(Team t){
+        void setTaem(Team t){//駒がどのチームに所属しているのかを設定する関数
             team=t;
         }
-        int getx(){
+        int getx(){//駒のx座標を返す関数
             return x;
         }
-        int gety(){
+        int gety(){//駒のy座標を返す関数
             return y;
         }
-        string getname(){
+        string getname(){//駒の名前を返す関数
             return name;
         }
-        bool getisAlive(){
+        bool getisAlive(){//駒が生きているかを返す関数
             return isAlive;
         }
-        Team getTeam(){
+        Team getTeam(){//駒がどのチームに所属しているかを返す関数
             return team;
         }
-        Role getRole(){
+        Role getRole(){//駒の役割を返す関数
             return role;
         }
-        void Stolen(){
+        void Stolen(){//駒が死んでいることを記録する関数
             isAlive=false;
         }
-        void Put(){
+        void Put(){//新しく駒が置かれたときにその駒が生きていることを記録する関数
             isAlive=true;
         }
-        void isNaru(){
+        void rePut(){//最初に駒が置かれたときに成らないのでfalseを代入する関数
+            naruState=false;
+        }
+        void isNaru(){//駒が成っていることを記憶して名前を変える関数
             naruState=true;
             switch(this->role){
                     case Hu     : name='と';break;
