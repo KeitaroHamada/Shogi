3a9234b4c0e5872ce4aa85f4ab0c42a6141ac969
KairiGit <130728369+KairiGit@users.noreply.github.com>
2023-06-19 11:33:22 +0900

    class Move

--- move.cpp
+++ move.cpp
@@ -1,21 +1,33 @@
 #include<iostream>
 #include<cstring>
 
+class Move{
+    private:
+        Koma k[9][9];
 
-void Take(Koma* p,Koma* q){
-    Koma tmp=*p;
-    *p=*q;
-    *q=tmp;
-}
-
-//駒の配列の先頭アドレス、移動前の座標と移動後の座標を受け取り、配列の中身を入れ替える。
-void Move(Koma* k,int x,int y,int X,int Y){
-    Koma* A=k[x][y];
-    Koma* C=k[X][Y];
-    if(C->getRole()=="Ou"){
-        cout <<"You Win"<<endl;
+        void Take(Koma* p,Koma* q){
+            Koma tmp=*p;
+            *p=*q;
+            *q=tmp;
+        }
+        void wincheck(Koma x){
+            if(k.getRole()=="Ou"){
+                cout <<"You Win"<<endl;
+            }
+        }
+    public:
+    Move(Koma* koma){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                k[i][j]=koma[i][j];
+            }
+        }
     }
-    else{
+//駒の配列の先頭アドレス、移動前の座標と移動後の座標を受け取り、配列の中身を入れ替える。
+    void move(int x,int y,int X,int Y){
+        Koma* A=k[x][y];
+        Koma* C=k[X][Y];
+        wincheck(*C);
         if(!A->getisAlive()){
             A->put();
         }
@@ -23,9 +35,5 @@ void Move(Koma* k,int x,int y,int X,int Y){
             C->Stolen();
         }
         Take(A,C);
-        k[x][y]=A;
-        k[X][Y]=C;
-        
     }
-    
 }
