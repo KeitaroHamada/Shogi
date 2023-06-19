3256c6c6ff1a9f2001dad82885d9159e03c389f7
KairiGit <r04i39@ed.cc.suzuka-ct.ac.jp>
2023-06-19 22:15:43 +0900

    a

--- move.cpp
+++ move.cpp
@@ -38,55 +38,35 @@ class Koma{
         }
 };
 
+
+void Take(Koma* p,Koma* q){
+    int tmpPx = p->getx();
+    int tmpPy = p->gety();
+    p->setPosition(q->getx(),q->gety());
+    q->setPosition(tmpPx,tmpPy);
+}
+void motiChange(Koma* r,Koma* i){
+    Role tmpr = r->getRole();
+    r->setName(i->getRole());
+    i->setName(tmpr);
+    r->Stolen();
+    i->put();
+}
 void Move(Koma* A,Koma* a,Koma* B,Koma* b){
     if(B->getRole()=="Ou"){
         cout <<"You Win"<<endl;
     }
     else{
-        int tmpAx = A->getx();
-        int tmpAy = A->gety();
+        if(A->isAlive()){
+            motiCange(A,a);
+        }
 
-        int tmpax = a->getx();
-        int tmpay = a->gety();
-
-        A->setPosition(B->getx(),B->gety());
-        B->setPosition(tmpAx,tmpAy);
-
-        a->setPosition(b->getx(),b->gety());
-        b->setPosition(tmpax,tmpay);
+        Take(A,B);
+        Take(a,b);
 
         if(B->isAlive()){
-            Role tmpBR = B->getRole();
-            B->setName(b->getRole());
-            b->setName(tmpBR);
-            B->Stolen();
-            b->put();
+            motiChange(B,b);
         }
     }
     
-}
-
-if(tmpb!='e'&&tmpb!='o'){//移動先を持ち駒に入れる
-        *motigoma = tmpb;
-    }
-    if(tmpb=='o'){
-        cout <<"You Win!"<<endl;
-    }
-
-int main(){
-    Koma ban[9][9];
-    Koma a('h');
-    Koma b('k');
-    Koma c('o');
-    char motigoma='m';
-
-    ban[0][3] = a;
-    ban[0][2] = b;
-    ban[0][1] = c;
-
-    Move(&a,&b,&motigoma);
-    cout << a.getname()<<b.getname()<<c.getname()<<motigoma<<endl;
-    Move(&b,&c,&motigoma);
-    cout << a.getname()<<b.getname()<<c.getname()<<motigoma<<endl;
-
 }
