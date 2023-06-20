602ad914ead27f2d3c12131e0fcfc130743947c0
masuyayout <m40ta2726@icluod.com>
2023-06-20 22:08:28 +0900

    関数の書き方をメモしました

--- makeKoma.h
+++ makeKoma.h
@@ -18,12 +18,13 @@ class Koma{
     public:
         void setPosition(const int x, const int y);//駒の座標を設定する関数
         void setTaem(const Team t);//駒がどのチームに所属しているのかを設定する関数
-        int getx();//駒のx座標を返す関数
-        int gety();//駒のy座標を返す関数
+        int getX();//駒のx座標を返す関数
+        int getY();//駒のy座標を返す関数
         bool getisAlive();//駒が生きているかを返す関数
         Team getTeam();//駒がどのチームに所属しているかを返す関数
         void Stolen();//駒が死んでいることを記録する関数
         void Put();//新しく駒が置かれたときにその駒が生きていることを記録する関数
+        void rePut();//最初に駒が置かれたときに成らないのでfalseを代入する関数
 };
 class makeRole{
     private:
@@ -36,5 +37,4 @@ class makeRole{
         Role getRole();//駒の役割を返す関数
         void isNaru();//駒が成っていることを記憶して名前を変える関数
         void rePut();//最初に駒が置かれたときに成らないのでfalseを代入する関数
-        void changeTeam();//駒が取られたときにチームを変更する関数class Komaから呼び出される
 };
