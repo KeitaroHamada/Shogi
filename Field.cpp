589f35d2384d1b8c99ff6f9acd2c5b51d8176b4e
KeitaroHamada <hamataro1029@gmail.com>
2023-06-21 16:04:55 +0900

    持ち駒の表示

--- Field.cpp
+++ Field.cpp
@@ -17,16 +17,31 @@ void Field::showField(){
                 k[koma[i][j].getX()][koma[i][j].getY()]=koma[i][j];
             }
         }
+        for(int j=0;j<9;j++){
+            for(int i=0;i<9;i++){
+                if(!k[i][j].isAlive() && k[i][j].getTeam==team2){
+                    cout << k[i][j].getName();
+                }
+            }
+        }
         cout << "９８７６５４３２１ \n";
         for(int j=0;j<9;j++){
             for(int i=0;i<9;i++){
                 if(!k[i][j].isAlive()){
                     cout << "　";
-                }else if(k[i][j].getTeam()==team1&&k[i][j].isAlive()){
+                }else if(k[i][j].getTeam() == team1&&k[i][j].isAlive()){
                     cout << "\x1b[33m" << k[i][j].getName() << "\x1b[0m";//もしteam1なら黄色で出力
                 }
                 else if(k[i][j].isAlive()){cout << k[i][j].getName();}
             }
             cout << kansuuji[j] << endl;
         }
+        for(int j=0;j<9;j++){
+            for(int i=0;i<9;i++){
+                if(!k[i][j].isAlive() && k[i][j].getTeam==team1){
+                    cout << "\x1b[33m" << k[i][j].getName() << "\x1b[0m";
+                }
+            }
+        }
+        cout << endl;
 }
